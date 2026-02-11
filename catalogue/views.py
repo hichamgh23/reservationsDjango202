0178dcf9be65ce12ae12422554bdcd18d1454758
Hicham Ghassoul <hicham.ghassoul2018@gmail.com>
2026-02-11 22:14:25 +0100

    Finalize show_detail template with representations and locations

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Artist
-from .models import Show  
+from .models import Artist, Show
 
+# Vue pour la liste des artistes
 def index(request):
     artists = Artist.objects.all()
     return render(request, 'catalogue/index.html', {'artists': artists})
 
-
-
+# Vue pour la liste des spectacles (Chapitre 9)
 def show_index(request):
     shows = Show.objects.all()
     return render(request, 'catalogue/show_index.html', {'shows': shows})
 
+# Vue pour le détail d'un spectacle (Page individuelle)
 def show_detail(request, show_id):
+    # On récupère le spectacle par son ID ou on affiche une erreur 404
     show = get_object_or_404(Show, id=show_id)
     return render(request, 'catalogue/show_detail.html', {'show': show})
